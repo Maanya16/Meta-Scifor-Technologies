67acb2677537384a22158e30be94d9f1a3e41dde
Maanya Singh <164649620+Maanya16@users.noreply.github.com>
2024-10-14 20:34:59 +0530

    Delete mini project 2 directory

--- mini project 2/ocr/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-     path("home/", views.hello, name="home"),
-    path("pdf/", views.pdf_to_txt, name="pdf"),
-    path("read/", views.ReadPdf, name="read"),
-
-]
